931dd187475ff2b076f7dfe153a509d5097ecddc
Justyna Gajda <justyna.gajda@vp.pl>
2020-08-11 22:18:13 +0200

    add reverse triangle

--- program_structure/main.js
+++ program_structure/main.js
@@ -1,22 +1,41 @@
-/*---------------Looping a triangle-----------------*/ 
+/*---------------Looping a triangle-----------------*/
 
 
-// let triangle = "O";
-// while (triangle.length <= 7) {
-//   console.log(triangle);
-//   triangle += "O";
-// }
-
-/*-OR-*/ 
+let triangle = "O";
+while (triangle.length <= 7) {
+  console.log(triangle);
+  triangle += "O";
+}
 
 
-// for (let result = "#"; result.length <=7; result = result + "#") {
-//   console.log(result);
-// }
+/*-OR-*/
+
+
+for (let result = "#"; result.length <=7; result = result + "#") {
+  console.log(result);
+}
 
 
 
-/*---------------FizzBuzz-----------------*/ 
+/*-Reverse triangle-*/
+
+let numberOfRows = 7; 
+let numberOfColumn = 7; 
+
+let reverseTriangle = "";
+
+for (let i = 0; i < numberOfColumn; i++) {
+  for (let j = 0; j < numberOfRows; j++) {
+    if (j >= i) {
+      reverseTriangle = reverseTriangle.concat("*");
+    }
+  }
+  reverseTriangle = reverseTriangle.concat("\n")
+}
+
+console.log(reverseTriangle)
+
+/*---------------FizzBuzz-----------------*/
 
 // for (let number = 1; number <= 100; number = number + 1)
 // if (number % 5 == 0 && number % 3 == 0) {
@@ -32,12 +51,12 @@
 
 
 
-/*---------------Chessboard-----------------*/ 
+/*---------------Chessboard-----------------*/
 
 
-let empty = "";
+// let empty = "";
 
-/*-Pojedynczy rząd-*/ 
+/*-Pojedynczy rząd-*/
 
 // for (let x = 0; x < 8; x++) { //ilosc kolumn
 //   if (x % 2 == 0) { 
@@ -47,17 +66,17 @@ let empty = "";
 //   }
 // }
 
-/*-Szachownica-*/ 
+/*-Szachwy-*/
 
-for (let y = 0; y < 8; y++) {
-  for (let x = 0; x < 8; x++) {
-    if ((x + y) % 2 == 0) {
-      empty += " ";
-    } else {
-      empty += "#";
-    }
-  }
-  empty += "\n";
-}
+// for (let y = 0; y < 8; y++) {
+//   for (let x = 0; x < 8; x++) {
+//     if ((x + y) % 2 == 0) {
+//       empty += " ";
+//     } else {
+//       empty += "#";
+//     }
+//   }
+//   empty += "\n";
+// }
 
-console.log(empty);
+// console.log(empty);
